97e4b79e741a1ab7fb10e1fc626178e8465cc994
SantinoMajur <dem65109@gmail.com>
2022-06-02 12:36:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
+int main(void){
+  
   char mystr[52] = "\"Programming is like building a multilingual puzzle";
+
   puts(mystr);
+
   return (0);
 }
