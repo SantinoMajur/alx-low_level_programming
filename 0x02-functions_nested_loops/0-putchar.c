619792dd7ca1f0d34f415f60c1a5742c22adfaf1
SantinoMajur <dem65109@gmail.com>
2022-06-08 03:07:58 -0700

    fixed typo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_putchar - prints characters _putchar
@@ -6,7 +7,7 @@
 int print_putchar(void)
 {
 	int i;
-	char myChar[9];
+	char myChar[9] = "_putchar";
 
 	for (i = 0; i < 8; i++)
 	{
