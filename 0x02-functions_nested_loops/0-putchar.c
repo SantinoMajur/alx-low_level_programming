78f51af733011b403ee764d7aed85b63e23a348c
SantinoMajur <dem65109@gmail.com>
2022-06-08 02:43:57 -0700

    function to print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_putchar - prints characters _putchar
+ * Return: (0) always
+ */
+int print_putchar(void)
+{
+	int i;
+	char myChar[9];
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(myChar[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-int _putchar(char c);
+int print_putchar(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
