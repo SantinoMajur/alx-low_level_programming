1e688b7eea582267362fae5b42fee419c67e3eea
SantinoMajur <dem65109@gmail.com>
2022-06-09 11:53:37 -0700

    betty checks passed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,17 +10,13 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incomming :(\n");
-
 	i = 0;
-
 	/**
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinte loop avoided! \\o/\n");
-
 	return (0);
 }
