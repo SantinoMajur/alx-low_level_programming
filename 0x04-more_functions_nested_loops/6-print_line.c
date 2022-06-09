e6fe7902a1b75f7367914accb1d5f79a8cad732f
SantinoMajur <dem65109@gmail.com>
2022-06-09 14:09:59 -0700

    added curly brackets

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,17 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
-		putchar('\n');
-	i = 0;
-	while (i <= n)
 	{
-		putchar('_');
-		i++;
+		putchar('\n');
 	}
-	putchar('\n');
+	else
+	{
+		i = 0;
+		while (i <= n)
+		{
+			putchar('_');
+			i++;
+		}
+		putchar('\n');
+	}
 }
