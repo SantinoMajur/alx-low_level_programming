e572516befe080f90dad16f007163db506f45d28
SantinoMajur <dem65109@gmail.com>
2022-06-09 12:53:43 -0700

    function print 0-9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>
 
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif /* MAIN_H */
