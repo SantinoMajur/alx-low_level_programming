a36c180aa9aa588ac5d8f99273822782038e192a
SantinoMajur <dem65109@gmail.com>
2022-06-09 14:21:08 -0700

    putchar

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('_');
+			putchar('_');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
