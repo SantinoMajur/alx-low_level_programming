97785bd8a6e70f73bd818d52a25f1e0924b54acf
SantinoMajur <dem65109@gmail.com>
2022-06-09 14:18:25 -0700

    fixed typos

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
 		for (i = 0; i <= n; i++)
 		{
-			putchar('_');
+			_putchar('_');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
