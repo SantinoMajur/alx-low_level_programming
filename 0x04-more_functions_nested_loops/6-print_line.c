87a3e26d39531672a1524b7f8e8215a44fbb9abd
SantinoMajur <dem65109@gmail.com>
2022-06-09 14:04:32 -0700

    print a straingh line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: holds input to print '_'
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		putchar('\n');
+	i = 0;
+	while (i <= n)
+	{
+		putchar('_');
+		i++;
+	}
+	putchar('\n');
+}
