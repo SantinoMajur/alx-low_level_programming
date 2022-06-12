18e92979c0b0ee8d09fbae378ec001440793a043
SantinoMajur <dem65109@gmail.com>
2022-06-12 02:22:57 -0700

    passed betty checks

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * main - prints alphabets in lowercase followed by a newline
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-  char myChar;
-    
-  for (myChar = 'a'; myChar <= 'z'; myChar++)
-    {
-      putchar(myChar);
-    }
-  putchar('\n');
-  return (0);
+	char myChar;
+
+	for (myChar = 'a'; myChar <= 'z'; myChar++)
+	{
+		putchar(myChar);
+	}
+	putchar('\n');
+	return (0);
 }
