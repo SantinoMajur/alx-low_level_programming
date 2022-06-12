0f564f62092a8a6e49c26500b6af1e0c49b7c6dc
SantinoMajur <dem65109@gmail.com>
2022-06-12 04:20:27 -0700

    added a second for loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,18 @@
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i != 99)
+		for (j = i + 1; j < 10; j++)
 		{
+			putchar((i % 10) + '0');
+			putchar((i % 10) + '0');
+			if ((i == 8) && (j == 9))
+			{
+				continue;
+			}
 			putchar(',');
 			putchar(' ');
 		}
