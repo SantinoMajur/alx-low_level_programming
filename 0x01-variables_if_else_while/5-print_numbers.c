f6f4827f626e81b1faf2bac190fbfbb0ac5aaed2
SantinoMajur <dem65109@gmail.com>
2022-06-12 02:47:22 -0700

    passed betty checks

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+/**
+ * main - prints single digit of base 10 starting from 0
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-  int num;
-  
-  for(num = 0; num < 10; num++)
-    {
-      printf("%d", num);
-    }
-  printf("\n");
-  return (0);
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
 }
