fd999a11f780f045b0660735077bdf4429709346
SantinoMajur <dem65109@gmail.com>
2022-06-12 02:54:49 -0700

    passed betty test

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - prints numbers from 0 using putchar
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-  int num;
-  
-  for(num = 0; num < 10; num++)
-    {
-      printf("%d", num);
-    }
-  printf("\n");
-  return (0);
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
 }
