cc1cb652c9a5a2f4ae800fae498fcf179cf998dc
SantinoMajur <dem65109@gmail.com>
2022-06-12 01:52:10 -0700

    passed betty checks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/* a c function to print a random positive and negative number */
+/**
+ * main - prints random negative or positive number
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-  int n;
+	int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      printf("%d is positive\n", n);
-    } else if (n == 0)
-    {
-      printf("%d is zero\n", n);
-    } else
-    {
-      printf("%d is negative\n", n);
-    }
-  return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
 }
