9deb0bbc525e784351deb051b3441126db070012
SantinoMajur <dem65109@gmail.com>
2022-06-13 09:20:18 -0700

    test successful locally

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints an array of integers followed by a newline
+ * @a: pointer int input, first input
+ * @n: second int input
+ *
+ * Return: return an array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i  = 0; i < (n - 1); i++)
+	{
+		printf("%d ", a[i]);
+		if (i == (n - 1))
+			printf("%d", a[n - 1]);
+	}
+	printtf("\n");
+}
