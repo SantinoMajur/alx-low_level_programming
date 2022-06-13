dded1e7f889927e853d18d98a82eaad6d36eb788
SantinoMajur <dem65109@gmail.com>
2022-06-13 09:06:11 -0700

    passed betty style checks

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string followed by a newline
+ * @str: string input
+ *
+ * Return: half of string
+ */
+void puts_half(char *str)
+{
+	int i, j, n;
+
+	n = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		n++;
+	}
+	n = (n / 2);
+
+	if ((n % 2) == 1)
+	{
+		j = ((n + 1) / 2);
+	}
+	for (i = j; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
