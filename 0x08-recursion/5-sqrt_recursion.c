44454b0548a3642526f7d6138a7a30572cfdfcb3
SantinoMajur <dem65109@gmail.com>
2022-06-30 15:22:49 -0700

    +sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: number input
+ * @i: input to find sqrt
+ * @r: square root
+ *
+ * Return: square root of a number
+ */
+int sqrt_root(int i, int r);
+int _sqrt_recursion(int n)
+{
+	int r;
+
+	r = 0;
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	return (sqrt_root(n, r));
+}
+/**
+ * sqrt_root - find square root of a number
+ * @i: input to find sqrt
+ * @r: square root
+ * @n: number input
+ *
+ * Return: square root or -1
+ */
+int sqrt_root(int i, int r)
+{
+	if ((r * r) == i)
+	{
+		return (r);
+	}
+	else if (r == (i / 2))
+	{
+		return (-1);
+	}
+	return (sqrt_root(i, r + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 
 #endif /* MAIN_H */
