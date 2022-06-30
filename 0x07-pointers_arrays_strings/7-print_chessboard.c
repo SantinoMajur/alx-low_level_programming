15a8c05ae2c87556622a183dd34cf4e9f2dbface
SantinoMajur <dem65109@gmail.com>
2022-06-30 13:04:09 -0700

    print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			putchar(a[i][j]);
+			j++;
+		}
+		i++;
+		putchar('\n');
+	}
+}
