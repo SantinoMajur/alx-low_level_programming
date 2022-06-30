c4d9a9e69df6fab34c1f301a4ee54879d9fa7608
SantinoMajur <dem65109@gmail.com>
2022-06-30 11:48:34 -0700

    strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,11 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (*(s + 1))
+	while (*(s + i))
 	{
 		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
-	if (*(s + i) == c)
-		return (s + i);
 	return (0);
 }
