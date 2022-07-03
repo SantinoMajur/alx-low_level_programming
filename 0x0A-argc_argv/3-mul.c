9f83296f2218ac38cb5756ba6ff3cde1d005910c
SantinoMajur <dem65109@gmail.com>
2022-07-03 05:04:00 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: number of arguments passed
@@ -12,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		puts("Error");
+		puts("Error\n");
 		return (1);
 	}
 	i = strtol(argv[1], NULL, 10);
