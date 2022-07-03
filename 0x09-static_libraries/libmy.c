d1fc343696746850a4cbee40794835655c97b4b1
SantinoMajur <dem65109@gmail.com>
2022-07-03 12:46:37 +0300

    added main.c

---
+++ 0x09-static_libraries/libmy.c
@@ -0,0 +1,215 @@
+#include "main.h"
+/**
+ * _putchar - puts a character
+ * @c: input character
+ * 
+ * Return: character
+ */
+int _putchar(char c)
+{
+
+}
+/**
+ * _islower - lowercase
+ * @c: input integer
+ * 
+ * Return: character
+ */
+int _islower(int c)
+{
+    
+}
+/**
+ * _isalpha - alphabet
+ * @c: input integer
+ * 
+ * Return: character
+ */
+int _isalpha(int c)
+{
+    
+}
+/**
+ * _abs - abs
+ * @n: input
+ * 
+ * Return: character
+ */
+int _abs(int n)
+{
+    
+}
+/**
+ * _isupper -  uppercase
+ * @c: input
+ * 
+ * Return: character
+ */
+int _isupper(int c)
+{
+    
+}
+/**
+ * _isdigit - check digit
+ * @c: input
+ * 
+ * Return: character
+ */
+int _isdigit(int c)
+{
+    
+}
+/**
+ * _strlen - length of string
+ * @s: input
+ * 
+ * Return: character
+ */
+int _strlen(char *s)
+{
+    
+}
+/**
+ * _puts - puts a character
+ * @s: input
+ * 
+ * Return: character
+ */
+void _puts(char *s)
+{
+    
+}
+/**
+ * _strcpy - copy a string
+ * @dest: input
+ * @src: second input
+ * 
+ * Return: character
+ */
+char *_strcpy(char *dest, char *src)
+{
+    
+}
+/**
+ * _atoi - puts a character
+ * @s: input
+ * 
+ * Return: character
+ */
+int _atoi(char *s)
+{
+    
+}
+/**
+ * _strcat - concatenate
+ * @dest: input
+ * @src: second input
+ * 
+ * Return: character
+ */
+char *_strcat(char *dest, char *src)
+{
+    
+}
+/**
+ * _strncat - concatenate
+ * @dest: input
+ * @src: second input
+ * @n: third input 
+ * Return: character
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    
+}
+/**
+ * _strncpy - copies
+ * @dest: input
+ * @src: second input
+ * @n: third input 
+ * 
+ * Return: character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    
+}
+/**
+ * _strcmp - compares
+ * @s1: input
+ * @s2: second input
+ * 
+ * Return: character
+ */
+int _strcmp(char *s1, char *s2)
+{
+    
+}
+/**
+ * _memset - copies a memory
+ * @s: input
+ * @b: input 2
+ * @n: int input
+ * 
+ * Return: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    
+}
+/**
+ * _memcpy - copies a memory
+ * @dest: input
+ * @src: input
+ * @n: input
+ * 
+ * Return: character
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    
+}
+/**
+ * _strchr - string character
+ * @s: input
+ * @c: second input
+ * 
+ * Return: character
+ */
+char *_strchr(char *s, char c)
+{
+    
+}
+/**
+ * _strspn - string
+ * @s: input
+ * @accept: second input
+ * 
+ * Return: character
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    
+}
+/**
+ * _strpbrk - puts a character
+ * @s: input
+ * @accept: second
+ * 
+ * Return: character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    
+}
+/**
+ * _strstr - puts a character
+ * @haystack: input
+ * @needle: second
+ * 
+ * Return: character
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    
+}
