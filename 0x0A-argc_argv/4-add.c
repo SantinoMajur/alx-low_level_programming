144b10ac072e25b74c1f099b433e3b0d3d2279fa
SantinoMajur <dem65109@gmail.com>
2022-07-03 05:23:39 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments passed
+ * @argv: arguments vector
+ *
+ * Return: success 0, error 1 followed by new line
+ */
+int main(int argc, char *argv[])
+{
+	int i, res, j;
+
+	i = 1;
+	res = 0;
+	j = 0;
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		j = strtol(argv[i], NULL, 10);
+		if (!j)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		res = res + j;
+	}
+	printf("%d\n", res);
+	return (0);
+}
