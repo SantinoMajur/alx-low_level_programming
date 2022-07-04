0c8099a9a0d80ad4c65f6a180c24716ea7d3370d
SantinoMajur <dem65109@gmail.com>
2022-07-04 07:27:06 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,17 +14,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-
 	size = max - min + 1;
-
 	array = malloc(sizeof(int) * size);
-
 	if (array == NULL)
 		return (NULL);
-
 	for (index = 0; index < size; index++)
 		array[index] = min++;
-
 	return (array);
 }
-Footer
