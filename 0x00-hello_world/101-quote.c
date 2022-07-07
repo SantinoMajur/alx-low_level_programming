da33ff83cd5eeebadf42c5c0a74aa7a822f9c1d9
SantinoMajur <dem65109@gmail.com>
2022-07-07 13:35:08 -0700

    betty checks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-  fprintf(stderr, "%s", "and that piece of art is usefull\"- Dora Korpar, 2015-10-19\n");
-  return 1;
+	printf("and that piece of art is usefull\"- Dora Korpar, 2015-10-19\n");
+	return (1);
 }
