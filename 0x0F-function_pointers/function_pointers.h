bac65a1f8a5df280936bad13599b2fe15ab51231
SantinoMajur <dem65109@gmail.com>
2022-07-07 21:16:50 +0300

    fixed typos

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,11 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
