38c16a03507a752a7e4db12c66d14cbe7d5044f6
SantinoMajur <dem65109@gmail.com>
2022-07-07 12:46:01 -0700

    passed betty checks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
-#include <stdio.h> 
-#include <string.h> 
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints strings followed by a new line
+ *
+ * Return: always 0
+ */
 int main(void)
-{   
+{
+	char mystr[52] = "\"Programming is like building a multilingual puzzle \n";
 
-  char mystr[52] = "\"Programming is like building a multilingual puzzle";
-  
-  puts(mystr);                            
-
-  return (0);
+	puts(mystr);
+	return (0);
 }
