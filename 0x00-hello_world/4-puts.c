cc39f10bdb8749edd772c9b808d69d6abc10dc92
SantinoMajur <dem65109@gmail.com>
2022-07-07 12:47:26 -0700

    passed betty checks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char mystr[52] = "\"Programming is like building a multilingual puzzle \n";
+	char mystr[52] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystr);
 	return (0);
